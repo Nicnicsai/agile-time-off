c3b0baac2e5b5f3215d8873a43d9ff5385d1ccc2
ALAA <alaashokri93@gmail.com>
2019-09-18 15:33:17 +0200

    update4

--- assets/JAVASCRIPT/time-off.js
+++ assets/JAVASCRIPT/time-off.js
@@ -1,15 +1,37 @@
 
-(function () {
-
-    document.getElementById("run").addEventListener("click", function () {
-
-
-
-                 document.getElementById('city-img').src = 'https://gocity.com/new-york/sites/g/files/ufrfmb171/files/styles/tile/public/New-York-skyline_0.jpg';
-                 document.getElementById('city-name').innerText = 'NEW YORK';
-                 document.getElementById('city-info').innerText = 'I was just trying to sudo apt update and sudo apt upgrade when an error with one of the packages (linux-headers-4.15.0-64-generic) started to appear, saying something about unmet dependencies. After ...';
-
-
+ (function fillTemplate () {
+//
+     document.getElementById("run").addEventListener("click", function () {
+//
+//l
+//
+               document.getElementById('city-img').src = 'https://gocity.com/new-york/sites/g/files/ufrfmb171/files/styles/tile/public/New-York-skyline_0.jpg';
+               document.getElementById('city-name').innerText = 'NEW YORK';
+               document.getElementById('city-info').innerText = 'I was just trying to sudo apt update and sudo apt upgrade when an error with one of the packages (linux-headers-4.15.0-64-generic) started to appear, saying something about unmet dependencies. After ...';
+//
+//
     });
 
-})();
+ })();
+
+/*
+const btn = document.querySelector('#randomizeButton');
+const results = {
+    machine1: document.querySelector('#machine1Result'),
+    machine2: document.querySelector('#machine2Result'),
+    machine3: document.querySelector('#machine3Result')
+};
+const el1 = document.querySelector('#machine1');
+const el2 = document.querySelector('#machine2');
+const el3 = document.querySelector('#machine3');
+const machine1 = new SlotMachine(el1, { active: 0 });
+const machine2 = new SlotMachine(el2, { active: 1 });
+const machine3 = new SlotMachine(el3, { active: 2 });
+function onComplete(active){
+    results[this.element.id].innerText = `Index: ${this.active}`;
+}
+btn.addEventListener('click', () => {
+    machine1.shuffle(5, onComplete);
+    setTimeout(() => machine2.shuffle(5, onComplete), 500);
+    setTimeout(() => machine3.shuffle(5, onComplete), 1000);
+});*/
