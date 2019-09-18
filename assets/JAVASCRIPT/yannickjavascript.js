b5f590641416e5cf8174aa23f6024bb2082de2e6
vindevoghel <yvindevoghel @gmail.com>
2019-09-18 14:07:12 +0200

    fixed fetches + returns random country

--- assets/JAVASCRIPT/yannickjavascript.js
+++ assets/JAVASCRIPT/yannickjavascript.js
@@ -1,5 +1,7 @@
 let dropDownInput = "europe";
 
+fetchCountry(dropDownInput);
+
 function fetchCountry(continent) {
     fetch("https://restcountries.eu/rest/v2/region/" + continent + "?fields=name;alpha2Code;capital")
         .then(function (response) {
@@ -7,18 +9,29 @@ function fetchCountry(continent) {
         })
         .then(function (countryInfo) {
             randomCountry = countryInfo[Math.floor((Math.random() * countryInfo.length))];
-            countryName = randomCountry.name;
-            countryCapital = randomCountry.capital;
-            alpha2Code = randomCountry.alpha2Code;
-            console.log("Country = " + countryName + ". City = " + countryCapital + ". Country code = " + alpha2Code);
-            })}
+            fetchCountryDescription(randomCountry.name);
+            fetchAttractions(randomCountry.name, randomCountry.capital);
+            return randomCountry;
+        })
+}
 
-fetchCountry(dropDownInput);
-
-function fetchAttractions(country, capital) {
-    fetch("https://api-gateway-becode.herokuapp.com/?goto=https://atlas-obscura-api.herokuapp.com/api/atlas/attractions/"+ country + "?city=" + capital)
+function fetchCountryDescription(country) {
+    fetch("https://api-gateway-becode.herokuapp.com/?goto=https%3A%2F%2Fen.wikipedia.org%2Fw%2Fapi.php%3Fformat%3Djson%26action%3Dquery%26prop%3Dextracts%26exintro%26explaintext%26redirects%3D1%26titles%3D" + country)
         .then(function (wikiresponse) {
             return wikiresponse.json();
+        })
+        .then(function (wikiData) {
+            let obj = wikiData.query.pages,
+                key = Object.keys(obj)[0],
+                extract = obj[key].extract;
+            console.log(extract);
+        })
+}
+
+function fetchAttractions(country, capital) {
+    fetch("https://api-gateway-becode.herokuapp.com/?goto=https://atlas-obscura-api.herokuapp.com/api/atlas/attractions/" + country + "?city=" + capital + "&limit=10")
+        .then(function (obscuraresponse) {
+            return obscuraresponse.json();
         })
         .then(function (obscuraData) {
             console.log(obscuraData);
@@ -26,8 +39,6 @@ function fetchAttractions(country, capital) {
             console.log(obscuraData.Attractions[0].description);
         })
 }
-
-fetchAttractions(countryName, countryCapital);
 
 /*
 async function getCapital(dropDownInput){
