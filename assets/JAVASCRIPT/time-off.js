b66ec54f817b4dd21650177336e8612bf430c05e
ALAA <alaashokri93@gmail.com>
2019-09-18 14:38:39 +0200

    update3

--- assets/JAVASCRIPT/time-off.js
+++ assets/JAVASCRIPT/time-off.js
@@ -1,1 +1,15 @@
 
+(function () {
+
+    document.getElementById("run").addEventListener("click", function () {
+
+
+
+                 document.getElementById('city-img').src = 'https://gocity.com/new-york/sites/g/files/ufrfmb171/files/styles/tile/public/New-York-skyline_0.jpg';
+                 document.getElementById('city-name').innerText = 'NEW YORK';
+                 document.getElementById('city-info').innerText = 'I was just trying to sudo apt update and sudo apt upgrade when an error with one of the packages (linux-headers-4.15.0-64-generic) started to appear, saying something about unmet dependencies. After ...';
+
+
+    });
+
+})();
