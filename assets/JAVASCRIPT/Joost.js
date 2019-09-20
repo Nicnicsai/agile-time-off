e4fa0f2e3e9129ee938fd8e0a23fe79b7ed02145
joostvannieu <joost.vannieuwenhuyse@gmail.com>
2019-09-20 10:08:02 +0200

    cleanup of Joost.js

--- assets/JAVASCRIPT/Joost.js
+++ assets/JAVASCRIPT/Joost.js
@@ -4,57 +4,21 @@ const ACTIVITIES_RADIUS = 5000; // radius from origin point in which to search f
 const RATING = 2; //minimal rating of PoI, range from 1 (lowest) - 3 (highest), possible to limit to heritage PoI by using 1h - 3h
 const MAX_RESULTS = 2; //the maximum amount of returned results
 const IMG_URL_PREPEND = "https://api-gateway-becode.herokuapp.com/?goto=";
-//const IMG_URL_PT1 = "https://";
 const IMG_URL_PT2 = ".wikipedia.org/w/api.php?action=query&format=json&formatversion=2&utf8=&prop=pageimages|pageterms&piprop=original&titles=";
 const IMG_URL_PT1_LENGTH = "https://fr".length;
 const IMG_URL_PT3_NEGATIVE_LENGTH = "https://fr.wikipedia.org/wiki/".length;
 
 //ACTIVITY TYPES
-let kinds = "&kinds=cultural,food"; //setting of kinds
-kinds = ""; //resetting of kinds
+const KINDS = ""; //setting of kinds, if you want it defined: const KINDS = "&kinds=cultural,food";
 
-//FUNCTIONS
-
-//Placeholder inputs
-//let cities = ["Antwerpen", "Chicago", "Capetown"];
-//let testXid = "R4156906";
-//let lon = 4.40346;
-//let lat = 51.21989;
-//let dropDownInput = "europe";
-
-/*
-//
-//let fetchString = `${FETCH_STRING_PREPEND}geoname?name=${getCity(cities)}&apikey=${API_KEY}`;
-//let fetchString = `${FETCH_STRING_PREPEND}bbox?lon_min=4.40346&lat_min=51.21989&lon_max=4.60346&lat_max=51.31989&kinds=restaurants&format=geojson&apikey=${API_KEY}`; //fetches restaurants in bounding box area
-//let fetchString = `${FETCH_STRING_PREPEND}xid/${testXid}?apikey=${API_KEY}`;
-//let fetchString = `${FETCH_STRING_PREPEND}xid/Q372040?apikey=${API_KEY}`;
-//let fetchString = `${FETCH_STRING_PREPEND}radius?radius=500&lon=${lon.toString()}&lat=${lat.toString()}&apikey=${API_KEY}`;
-*/
-/*
-//Basic Fetch Data function
-function fetchData(fetchString){
-    fetch(fetchString)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (touristInfo) {
-            console.log(touristInfo)
-        })
-}
-//fetchData(fetchString);*/
-
-/*
-//deprecated
-function getCity(cities) {
-    return cities[Math.round(Math.random()*(cities.length-1))];
-}*/
-//console.log(getCity(cities));
-
+//FUNCTIONS //these will all be triggered in a waterfall way from the yannickjavascript2.js file
+//Eventually gets te activities in the given city
 function getFunStuff(city) {
     let fetchString = `${FETCH_STRING_PREPEND}geoname?name=${city}&apikey=${API_KEY}`;
     fetchCoordinates(fetchString);
 }
 
+//Gets the longitude and latitude of the given city
 function fetchCoordinates(fetchString){
     fetch(fetchString)
         .then(function (response) {
@@ -68,24 +32,22 @@ function fetchCoordinates(fetchString){
         })
 }
 
+//Fetches the activity XID, a unique specifier given through the OpenTripMap api
 function fetchActivities(lon, lat){
-    let fetchString = `${FETCH_STRING_PREPEND}radius?radius=${ACTIVITIES_RADIUS}&lon=${lon.toString()}&lat=${lat.toString()}${kinds}&rate=${RATING}&limit=${MAX_RESULTS}&apikey=${API_KEY}`;
+    let fetchString = `${FETCH_STRING_PREPEND}radius?radius=${ACTIVITIES_RADIUS}&lon=${lon.toString()}&lat=${lat.toString()}${KINDS}&rate=${RATING}&limit=${MAX_RESULTS}&apikey=${API_KEY}`;
 
     fetch(fetchString)
         .then(function (response) {
             return response.json();
         })
         .then(function (activities) {
-            //console.log(activities);//show generated list of activities
             activities.features.forEach(function (activity) {
-                //console.log(activity.properties.kinds);//show kinds of activity
-                //console.log(activity.properties.xid);//show xid of the activity
                 getActivityInfo(activity.properties.xid)
-
             })
         })
 }
 
+//Fetches the actual activity info (activity is a JSON object containing all the required info of said activity)
 function getActivityInfo(activityXid){
     let fetchString = `${FETCH_STRING_PREPEND}xid/${activityXid}?apikey=${API_KEY}`;
     fetch(fetchString)
@@ -93,26 +55,16 @@ function getActivityInfo(activityXid){
             return response.json();
         })
         .then(function (activity) {
-            /*console.log("NEW ACTIVITY");
-            console.log(activity);
-            console.log(activity.name);
-            console.log(activity.image);
-            console.log(activity.url);
-            console.log(activity.wikipedia_extracts.title);
-            console.log(activity.wikipedia_extracts.text);*/
-
             getWikiImgLink(activity);
-
-            //fillTemplate(activity.name, activity.image, activity.wikipedia_extracts.text, activity.url)
         })
 }
+
+//Fetches the image from the wikipedia article belonging to the activity
 function getWikiImgLink(activity) {
-    //console.log("wiki link");
-    //console.log(activity.wikipedia);
-    let imgUrlPt1 = activity.wikipedia.substr(0,IMG_URL_PT1_LENGTH);
+    //compiles the fetch string for the wikipedia rest api
+    let imgUrlPt1 = activity.wikipedia.substr(0, IMG_URL_PT1_LENGTH);
     let imgUrlPt3 = activity.wikipedia.substr(IMG_URL_PT3_NEGATIVE_LENGTH, activity.wikipedia.length-IMG_URL_PT3_NEGATIVE_LENGTH);
     let fetchString = IMG_URL_PREPEND + encodeURIComponent(imgUrlPt1 + IMG_URL_PT2 + imgUrlPt3);
-    //console.log(fetchString);
 
     fetch(fetchString)
         .then(function (response) {
@@ -128,36 +80,25 @@ function getWikiImgLink(activity) {
             citySlot.src = imgUrl;
             fillTemplate(activity.name, imgUrl, activity.wikipedia_extracts.text, activity.url)
         })
-
 }
 
+//Function to fill up the template in the HTML file
 function fillTemplate(title, imgUrl, description, url) {
-    let template = document.querySelector("#tpl-slotmachine");
-    let placementLocation = document.querySelector("#resultsDisplay");
-    //Need to prepend img link with : https://api-gateway-becode.herokuapp.com/?goto=
-    //In order to prevent CORB warnings / conflicts
-    //let adjustedImgUrl = IMG_URL_PREPEND.concat(encodeURI(imgUrl));
-    //let adjustedImgUrl = imgUrl;
-    //console.log(adjustedImgUrl);
+    const TEMPLATE = document.querySelector("#tpl-slotmachine");
+    const PLACEMENT_LOCATION = document.querySelector("#resultsDisplay");
 
-    template.content.querySelector(".title-activity").innerText = title;
-    template.content.querySelector(".img-activity").setAttribute("src", imgUrl);
-    template.content.querySelector(".description").innerText = description;
+    TEMPLATE.content.querySelector(".title-activity").innerText = title;
+    TEMPLATE.content.querySelector(".img-activity").setAttribute("src", imgUrl);
+    TEMPLATE.content.querySelector(".description").innerText = description;
 
     if (url !== undefined && url !== null) {
-        template.content.querySelector(".link").setAttribute("href", url);
-        template.content.querySelector(".link").innerText = url;
+        TEMPLATE.content.querySelector(".link").setAttribute("href", url);
+        TEMPLATE.content.querySelector(".link").innerText = url;
     } else {
-        template.content.querySelector(".link").setAttribute("href", "");
-        template.content.querySelector(".link").innerText = "";
+        TEMPLATE.content.querySelector(".link").setAttribute("href", "");
+        TEMPLATE.content.querySelector(".link").innerText = "";
     }
 
-    let clone = template.content.cloneNode(true);
-    placementLocation.appendChild(clone);
+    let clone = TEMPLATE.content.cloneNode(true);
+    PLACEMENT_LOCATION.appendChild(clone);
 }
-
-/*getCapital(dropDownInput)
-    .then(capital => {
-        console.log(capital);
-        getCoordinates(capital)
-    });*/
